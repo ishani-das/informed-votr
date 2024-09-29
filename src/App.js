7f567f992da175b5ea2390d5eed8cfb7e299cd42
Ishani Das <ishanidas05@gmail.com>
2024-09-29 02:11:55 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,272 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import USAMap from 'react-usa-map';
+import StatePage from './StatePage';
+import BillPage from './BillPage';
+import './App.css';
+
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import FormHelperText from '@mui/joy/FormHelperText';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+
+class App extends React.Component {
+
+  HomePageCustomConfig = () => {
+    // https://www.270towin.com/
+    return {
+      "WA": { fill: "navy", }, "OR": { fill: "navy", }, "CA": { fill: "navy", }, "CO": { fill: "navy", }, "NM": { fill: "navy", }, "MN": { fill: "navy", },
+      "IL": { fill: "navy", }, "VA": { fill: "navy", }, "NY": { fill: "navy", }, "ME": { fill: "navy", }, "VT": { fill: "navy", }, "NH": { fill: "navy", },
+      "MA": { fill: "navy", }, "RI": { fill: "navy", }, "CT": { fill: "navy", }, "NJ": { fill: "navy", }, "DE": { fill: "navy", }, "MD": { fill: "navy", }, 
+      "DC": { fill: "navy", }, "HI": { fill: "navy", },
+
+      "ID": { fill: "red", }, "MT": { fill: "red", }, "WY": { fill: "red", }, "UT": { fill: "red", }, "ND": { fill: "red", }, "SD": { fill: "red", },
+      "NE": { fill: "red", }, "KS": { fill: "red", }, "IA": { fill: "red", }, "OK": { fill: "red", }, "MO": { fill: "red", },
+      "AR": { fill: "red", }, "TX": { fill: "red", }, "LA": { fill: "red", }, "MS": { fill: "red", }, "AL": { fill: "red", }, "GA": { fill: "red", },
+      "TN": { fill: "red", }, "SC": { fill: "red", }, "FL": { fill: "red", }, "NC": { fill: "red", }, "KY": { fill: "red", }, "WV": { fill: "red", }, 
+      "IN": { fill: "red", }, "OH": { fill: "red", }, "AK": { fill: "red", },
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage HomePageCustomConfig={this.HomePageCustomConfig} />} />
+          <Route path="/bills/:stateCode" element={<StatePage />} />
+          <Route path="/bill/:billId" element={<BillPage />} />
+        </Routes>
+      </Router>
+    )
+  }
+}
+
+const HomePage = ({ HomePageCustomConfig }) => {
+
+  // --------------------------------------------
+  const [data, setData] = React.useState({
+    email: '',
+    status: 'initial',
+  });
+
+  const handleSubmit = async (event) => {
+    // console.log(event);
+    event.preventDefault();
+    // setData((current) => ({ ...current, status: 'loading' }));
+    // try {
+    //   // Replace timeout with real backend operation
+    //   setTimeout(() => {
+    //     setData({ email: '', status: 'sent' });
+    //   }, 1500);
+    // } catch (error) {
+    //   setData((current) => ({ ...current, status: 'failure' }));
+    // }
+
+    // Update the status to loading
+    setData((prevData) => ({ ...prevData, status: 'loading' }));
+
+    try {
+      // Send email to the backend
+      // https://iv-backend-new-37160a2e20fa.herokuapp.com/
+      // const response = await fetch('http://localhost:5000/subscribe', {
+      const response = await fetch('https://iv-backend-new-37160a2e20fa.herokuapp.com/subscribe', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: data.email }),
+      });
+
+      if (response.ok) {
+        // Update status on success
+        setData((prevData) => ({ ...prevData, status: 'sent' }));
+      } else {
+        // Handle failure case
+        setData((prevData) => ({ ...prevData, status: 'failure' }));
+      }
+    } catch (error) {
+      // Handle network errors or unexpected issues
+      setData((prevData) => ({ ...prevData, status: 'failure' }));
+    }
+
+
+
+    // try {
+    //   const response = await fetch('http://127.0.0.1:5000/subscribe', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ email: data.email }),
+    //   });
+  
+    //   if (response.ok) {
+    //     setData({ email: '', status: 'sent' });
+    //   } else {
+    //     const errorData = await response.json();
+    //     setData({ email: '', status: 'failure', error: errorData.error });
+    //   }
+    // } catch (error) {
+    //   setData({ email: '', status: 'failure', error: 'Network error' });
+    // }
+  };
+  // --------------------------------------------
+
+
+  const navigate = useNavigate();
+
+  const mapHandler = (event) => {
+    const stateCode = event.target.dataset.name;
+    console.log(`${stateCode} CLICKED!`);
+    navigate(`/bills/${stateCode}`);
+  }
+
+  return (
+    <div style={{ paddingLeft: '20px', paddingRight: '20px' }}>
+      <br />
+      <h1>welcome! click on any state to get started.</h1>
+
+      <form onSubmit={handleSubmit} id="demo">
+      <FormControl>
+        <FormLabel
+          sx={(theme) => ({
+            '--FormLabel-color': theme.vars.palette.primary.plainColor,
+          })}
+        >
+          Join our Newsletter to receive weekly updates!
+        </FormLabel>
+        <Input
+          sx={{ '--Input-decoratorChildHeight': '45px' }}
+          placeholder="imavoter@gmail.com  -  CA, US"
+          type="email"
+          required
+          value={data.email}
+          onChange={(event) =>
+            setData({ email: event.target.value, status: 'initial' })
+          }
+          error={data.status === 'failure'}
+          endDecorator={
+            <Button
+              variant="solid"
+              color="primary"
+              loading={data.status === 'loading'}
+              type="submit"
+              sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+            >
+              Subscribe
+            </Button>
+          }
+        />
+        {data.status === 'failure' && (
+          <FormHelperText
+            sx={(theme) => ({ color: theme.vars.palette.danger[400] })}
+          >
+            Oops! Either you are already on the list or something went wrong. If the latter, please try again later.
+          </FormHelperText>
+        )}
+
+        {data.status === 'sent' && (
+          <FormHelperText
+            sx={(theme) => ({ color: theme.vars.palette.primary[400] })}
+          >
+            You're all set! Expect an email from us shortly.
+          </FormHelperText>
+        )}
+      </FormControl>
+    </form>
+
+
+
+      {/* <form onSubmit={handleSubmit} id="demo">
+      <FormControl>
+        <FormLabel
+          sx={(theme) => ({
+            '--FormLabel-color': theme.vars.palette.primary.plainColor,
+          })}
+        >
+          MUI Newsletter
+        </FormLabel>
+        <Input
+          sx={{ '--Input-decoratorChildHeight': '45px' }}
+          placeholder="mail@mui.com"
+          type="email"
+          required
+          value={data.email}
+          onChange={(event) =>
+            setData({ email: event.target.value, status: 'initial' })
+          }
+          error={data.status === 'failure'}
+          endDecorator={
+            <Button
+              variant="solid"
+              color="primary"
+              loading={data.status === 'loading'}
+              type="submit"
+              sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+            >
+              Subscribe
+            </Button>
+          }
+        />
+        {data.status === 'failure' && (
+          <FormHelperText
+            sx={(theme) => ({ color: theme.vars.palette.danger[400] })}
+          >
+            Oops! something went wrong, please try again later.
+          </FormHelperText>
+        )}
+        {data.status === 'sent' && (
+          <FormHelperText
+            sx={(theme) => ({ color: theme.vars.palette.primary[400] })}
+          >
+            You are all set!
+          </FormHelperText>
+        )}
+      </FormControl>
+    </form> */}
+
+
+
+
+
+
+
+
+
+      <div className="map-container">
+        <USAMap
+          customize={HomePageCustomConfig()}
+          onClick={mapHandler}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/BillPage.js
@@ -0,0 +1,206 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { styled } from "@mui/material/styles";
+import MuiButton from "@mui/material/Button";
+import { Typography, Container, CircularProgress, /*Button, */Card, CardMedia, CardContent, Grid, CardActionArea, Box } from '@mui/material';
+// import HtmlContent from './HtmlContent';
+import { htmlToText } from 'html-to-text';
+import { fetchSearchResults } from './googleSearchService';
+
+export const Button = styled(MuiButton)(({ pill }) => ({
+  borderRadius: pill ? 50 : 4,
+  // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', 
+  background: 'linear-gradient(45deg, #0ea2e6 30%, #bd4242 90%)',
+  color: 'white', 
+  // '&:hover': {
+  //   background: 'linear-gradient(45deg, #3470ad 30%, #0ea2e6 90%)', 
+  // },
+}));
+
+const BillPage = () => {
+  const { billId } = useParams();
+  const [billDetails, setBillDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [billTextInHTML, setBillTextInHTML] = useState(null);
+  const [summary, setSummary] = useState('');
+  const [summaryLoading, setSummaryLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const fetchBillDetails = async () => {
+      try {
+        const response = await axios.post(`https://api.legiscan.com/?key=6ef4e9b53f820698d5d6a447dc7499c7&op=getBill&id=${billId}`);
+        console.log('Bill details:', response.data.bill);
+        setBillDetails(response.data.bill);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching bill details:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchBillDetails();
+  }, [billId]);
+
+  useEffect(() => {
+    const fetchBillText = async () => {
+      if (billDetails && billDetails.texts && billDetails.texts.length > 0) {
+        const docId = billDetails.texts[billDetails.texts.length - 1].doc_id;
+        try {
+          const response = await axios.post(`https://api.legiscan.com/?key=6ef4e9b53f820698d5d6a447dc7499c7&op=getBillText&id=${docId}`);
+          console.log('Bill text details:', response.data);
+          /*
+          setBillText(response.data.text.text_hash);
+          let decodedText = atob(billText);
+          console.log("DECODED TEXT : ", decodedText);
+          */
+
+          // Example Base64 encoded string
+          const base64String = response.data.text.doc;
+          // Decode Base64 string
+          const decodedString = atob(base64String);
+          // const decodedString = Buffer.from(base64String, 'base64').toString('utf-8');
+          console.log(decodedString);  // Output: "Hello World"
+          setBillTextInHTML(decodedString);
+
+        } catch (error) {
+          console.error("Error fetching bill text details:", error);
+        }
+      }
+    };
+
+    fetchBillText();
+  }, [billDetails]);
+
+  useEffect(() => {
+    const fetchSearchResultsForBill = async () => {
+      if (billDetails) {
+        const results = await fetchSearchResults(billDetails.title);
+        setSearchResults(results);
+      }
+    };
+
+    fetchSearchResultsForBill();
+  }, [billDetails]);
+
+  const summarizeText = async () => {
+    if (!billTextInHTML) return;
+    setSummaryLoading(true);
+    try {
+      const plainText = htmlToText(billTextInHTML, { wordwrap: 130 });
+      console.log('PLAIN TEXT : ', plainText)
+      // const response = await axios.post('http://127.0.0.1:5000/summarize', { text: plainText }); 
+      // setSummary(response.data[0].summary_text);
+      // const response = await axios.post('http://127.0.0.1:5000/summarize_groq', { text: plainText }); 
+      const response = await axios.post('https://iv-backend-new-37160a2e20fa.herokuapp.com/summarize_groq', { text: plainText }); 
+      setSummary(response.data.summarized_text);
+    } catch (error) {
+      console.error('Error summarizing text:', error);
+      setSummary('Error summarizing text');
+    } finally {
+      setSummaryLoading(false);
+    }
+  };
+
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  if (!billDetails) {
+    return <Typography variant="h6">Bill details not found.</Typography>;
+  }
+
+  return (
+    <Container>
+      <br />
+      <br />
+      <Typography variant="h4" gutterBottom style={{ fontWeight: 'bold' }}>
+        {billDetails.bill_number} - <a href={billDetails.url} target="_blank" rel="noopener noreferrer">
+          {billDetails.title}
+        </a>
+      </Typography>
+      <Typography variant="h6" paragraph style={{ fontStyle: 'italic' }}>
+        {billDetails.description}
+      </Typography>
+      <br />
+      <Typography variant="h5" gutterBottom align="center" style={{ fontWeight: 'bold' }}>
+          Confused?
+      </Typography>
+      <br />
+      {billTextInHTML && (
+        <div>
+          <Grid container justifyContent="center" mb={2}>
+            <Button variant="outlined" pill size="large" onClick={summarizeText}
+            sx={{
+              padding: '12px 24px',  // Adjust padding as needed
+              fontSize: '1.2rem',    // Adjust font size as needed
+              fontWeight: 'bold',    // Make the text bold
+            }}
+            >
+            {/* <Button size = "large" variant="text" color="primary" onClick={summarizeText}> */}
+              Let us summarize it for you! ✨
+            </Button>
+          </Grid>
+          {summaryLoading && <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="50vh" // Optional: To center vertically in full viewport height
+            >
+            <CircularProgress />
+          </Box>}
+          {/* <HtmlContent html={billTextInHTML}/> */}
+         
+          {summary && (
+             <Typography sx={{ fontSize: '18px' }} color="textSecondary">
+              {/* <h2>Summary</h2> */}
+              <p>{summary}</p>
+            </Typography>
+          )}
+        </div>
+
+      )}
+      <br />
+      <br />
+      <br />
+      <Typography variant="h5" gutterBottom align="center" style={{ fontWeight: 'bold' }}>
+        Related Content in the Media
+      </Typography>
+      <Grid container spacing={2}>
+        {searchResults.map((result, index) => (
+          <Grid item xs={12} sm={6} md={4} key={result.cacheId || index}>
+            <Card 
+              style={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+              onClick={() => window.open(result.link, '_blank')}
+              >
+              <CardActionArea>
+                {result.pagemap && result.pagemap.cse_image && result.pagemap.cse_image.length > 0 && (
+                  <CardMedia
+                    component="img"
+                    alt={result.title}
+                    height="140"
+                    image={result.pagemap.cse_image[0].src}
+                  />
+                )}
+                <CardContent style={{ flexGrow: 1 }}>
+                  <Typography variant="h5" component="div">
+                    {result.title}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {result.snippet}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+
+
+    </Container>
+  );
+}
+
+export default BillPage;
